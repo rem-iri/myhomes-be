8cffe2edf8fbe842307333e690260fd7e741b1e1
Remer <remer.irineo@gmail.com>
2023-07-06 17:58:19 +0800

    Initial commit

---
+++ src/main/java/com/groupthree/myhomesbe/MyhomesBeApplication.java
@@ -0,0 +1,13 @@
+package com.groupthree.myhomesbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhomesBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupthree/myhomesbe/MyhomesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupthree.myhomesbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomesBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
